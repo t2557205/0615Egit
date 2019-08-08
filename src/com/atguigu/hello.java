3120e47be8437cf333dcf5754d8abf654bb95362
llq <53886748+t2557205@users.noreply.github.com>
2019-08-08 15:15:39 +0800

    Update hello.java

--- src/com/atguigu/hello.java
+++ src/com/atguigu/hello.java
@@ -5,6 +5,7 @@ public class hello {
 	public static void main(String []args) {
 		
 		System.out.println("这是一个程序！");
-		system.out.println("我新加了一个功能！");
+		System.out.println("我新加了一个功能！");
+		System.out.println("这是另一个程序！");
 	}
 }
